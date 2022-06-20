7bd69ca9b02c997ea724443889236296d5b25bf7
faruque5698 <ashaduzzaman5698@gmail.com>
2022-06-20 11:50:56 +0600

    User login Notification  done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,9 @@
         </nav>
 
         <main class="py-4">
+
+            <div id="notification" class="alert alert-success mx-3 invisible"></div>
+
             @yield('content')
         </main>
     </div>
