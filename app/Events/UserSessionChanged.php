f96c84ea5cad76ad8f1870f36a4d077cb660ced9
faruque5698 <ashaduzzaman5698@gmail.com>
2022-06-20 13:15:45 +0600

    Real Time Public Changel Notification Done

--- app/Events/UserSessionChanged.php
+++ app/Events/UserSessionChanged.php
@@ -36,8 +36,7 @@ class UserSessionChanged implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        \Log::debug($this->message);
-        \Log::debug($this->type);
-        return new PrivateChannel('notifications');
+
+        return new Channel('notifications');
     }
 }
